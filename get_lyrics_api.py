43bcf508f29f89359e6bf7e0d541754f805b8267
Antony Albuquerque <118316078+AlbuquerquexD@users.noreply.github.com>
2025-04-17 01:16:24 -0300

    Testando api lyrics ovh

--- get_lyrics_api.py
+++ get_lyrics_api.py
@@ -1,10 +1,26 @@
 import requests
-endpoint ="https://api.lyrics.ovh/v1/Seu jorge/Mina do Condomínio"
+import streamlit as st
 
-response = requests.get(endpoint)
+col1, col2, col3 = st.columns([3, 1, 2])
 
-key = list(response.json().keys())[0]
+with col1:
+    st.title("Pesquisar Lyrics (lyrics.ovh)")
+    busca_artista = st.text_input(label='Nome do artista')
+    busca_musica = st.text_input(label='Nome da música')
 
-data = response.json()[key]
+    endpoint = f"https://api.lyrics.ovh/v1/seu jorge/mina do condominio"
 
-print(data)
+    response = requests.get(endpoint)
+
+    key = list(response.json().keys())[0]
+
+    data = response.json()[key]
+
+    print(data)
+
+# with col2:
+#     st.write('Coluna2')
+
+with col3:
+    with st.container(height=600):
+        st.write(data)
